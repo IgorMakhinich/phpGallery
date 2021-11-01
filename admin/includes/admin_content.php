fd347f47ac00365cbd51b1c54f368d754e5e70b7
Igor Makhinich <igor.makhinich@gmail.com>
2021-11-01 15:25:32 +0200

    user class continue + jetbrains config

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,25 +1,34 @@
 <div id="layoutSidenav_content">
-	<main>
-		<div class="container-fluid px-4">
-			<h1 class="mt-4">Dashboard</h1>
-			<ol class="breadcrumb mb-4">
-				<li class="breadcrumb-item active"><a href="index.php">Dashboard</a></li>
-				<li class="breadcrumb-item"><a href="#">Blank page</a></li>
-			</ol>
-			<?php
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active"><a href="index.php">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="#">Blank page</a></li>
+            </ol>
+            <?php
 
-			$result_set = User::find_all_users();
+//            $result_set = User::find_all_users();
+//
+//            while ($row = mysqli_fetch_array($result_set)) {
+//                echo "ID: " . $row['id'] . ", Name: " . $row['username'] . "<br>";
+//            }
 
-			while ($row = mysqli_fetch_array($result_set)) {
-				echo "ID: " . $row['id'] . ", Name: " .$row['username'] . "<br>";
-			}
+//            echo '<hr>';
+//
+//            $user = User::find_user_by_id(1);
+//            echo "User id {$user['id']} is: <br>";
+//            print_r($user);
 
-			$user = User::find_user_by_id(2);
-			echo "User id 2 is: <br>";
-			print_r($user);
+            $users = User::find_all_users();
 
-			?>
-		</div>
-	</main>
-	<?php include "sub_footer.php"; ?>
+            foreach ($users as $user){
+                echo $user->username . "<br>";
+            }
+
+
+            ?>
+        </div>
+    </main>
+    <?php include "sub_footer.php"; ?>
 </div>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,19 +2,52 @@
 
 class User
 {
-	public static function find_all_users()
-	{
-		global $database;
-		$result_set = $database->query("SELECT * FROM users");
-		return $result_set;
-	}
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
 
-	public static function find_user_by_id($id)
-	{
-		global $database;
-		$sql = "SELECT * FROM users WHERE id = $id LIMIT 1";
-		$result_set = $database->query($sql);
-		$result = mysqli_fetch_assoc($result_set);
-		return $result;
-	}
+    public static function find_this_query($sql)
+    {
+        global $database;
+        $result_set = $database->query($sql);
+        $the_object_array = array();
+        while ($row = mysqli_fetch_array($result_set)) {
+            $the_object_array[] = self::instantiation($row);
+        }
+        return $the_object_array;
+    }
+
+    public static function find_all_users()
+    {
+//        global $database;
+//        return $database->query("SELECT * FROM users");
+        return self::find_this_query("SELECT * FROM users");
+    }
+
+    public static function find_user_by_id($id)
+    {
+        $result_set = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
+        return mysqli_fetch_assoc($result_set);
+    }
+
+    public static function instantiation($the_record)
+    {
+        $the_object = new self;
+
+        foreach ($the_record as $property => $value) {
+            if ($the_object->has_the_property($property)) {
+                $the_object->$property = $value;
+            }
+        }
+        return $the_object;
+    }
+
+    private function has_the_property($property)
+    {
+        $object_properties = get_object_vars($this);
+        return array_key_exists($property, $object_properties);
+    }
+
 }
