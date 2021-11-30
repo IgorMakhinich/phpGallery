164223a0d614a407b32fea3d66104cc95a82511b
Igor Makhinich <igor.makhinich@gmail.com>
2021-11-30 14:37:03 +0200

    adduser form add input

--- admin/add_user.php
+++ admin/add_user.php
@@ -31,7 +31,10 @@ if (isset($_POST['add'])) {
             <h1 class="mt-4">Add User</h1>
             <form action="" method="POST" enctype="multipart/form-data">
                <div class="row">
-                  <div class="col-md-8">
+                  <div class="col-md-6 offset-md-3">
+                     <div class="form-group">
+                        <input type="file" name="user_image">
+                     </div>
                      <div class="form-group">
                         <label for="username">Userame</label>
                         <input type="text" name="username" class="form-control">
@@ -48,9 +51,9 @@ if (isset($_POST['add'])) {
                         <label for="password">Password</label>
                         <input type="text" name="password" class="form-control">
                      </div>
-                  </div>
-                  <div class="info-box-update">
-                     <input type="submit" name="add" value="Add" class="btn btn-primary btn-lg ">
+                     <div class="info-box-update">
+                        <input type="submit" name="add" value="Add" class="btn btn-primary float-end mt-2">
+                     </div>
                   </div>
                </div>
             </form>
